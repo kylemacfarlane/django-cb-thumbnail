d07ab8e9dd7c729c18910b84b342f5e946cb70e9
Kyle MacFarlane <kyle@deletethetrees.com>
2011-02-08 15:46:07 +0000

    Bumped version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = 'django-cuddlybuddly-thumbnail',
-    version = '2.1',
+    version = '2.2',
     license = 'BSD',
     description = 'A Django app that supplies a template tag to generate ' \
                   'thumbnails. Specifically designed to be compatible with ' \
